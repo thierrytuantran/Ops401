f9785744351663706a53e50d8f8f34fbc4339329
Thierry Tran <131303287+thierrytuantran@users.noreply.github.com>
2024-03-05 23:59:17 -0800

    Create Ops42.py

---
+++ challenges/Ops42.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+import nmap
+
+scanner = nmap.PortScanner()
+
+print("Nmap Automation Tool")
+print("--------------------")
+
+ip_addr = input("IP address to scan: ")
+print("The IP you entered is: ", ip_addr)
+type(ip_addr)
+
+resp = input("""\nSelect scan to execute:
+                1) SYN ACK Scan
+                2) UDP Scan
+                3)              \n""") ### TODO: Select what your third scan type will be
+print("You have selected option: ", resp)
+
+range = '1-50'
+
+### TODO: Prompt the user to type in a port range for this tool to scan
+range = input("Enter port range to scan (e.g., 1-100): ")
+
+if resp == '1':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, range, '-v -sS')
+    print(scanner.scaninfo())
+    print("Ip Status: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
+elif resp == '2':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, range, '-v -sU')  # UDP scan
+    print(scanner.scaninfo())
+    print("Ip Status: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['udp'].keys())
+
+elif resp == '3':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, range, '-T4 -A')  # Intense scan
+    print(scanner.scaninfo())
+    print("Ip Status: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
+
+elif resp >= '4':
+    print("Please enter a valid option")
